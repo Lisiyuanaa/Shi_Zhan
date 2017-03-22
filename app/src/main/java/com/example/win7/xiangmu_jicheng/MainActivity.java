041bc0c25334513e4a2ca9a9bb7ccfb066cfe03a
chanima <1670715459@qq.com>
2017-03-22 11:49:09 +0800

    new pro

---
+++ app/src/main/java/com/example/win7/xiangmu_jicheng/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.win7.xiangmu_jicheng;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
