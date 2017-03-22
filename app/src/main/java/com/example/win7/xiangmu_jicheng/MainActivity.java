de39f3c67081d26920e5e5c861c91bec4a977b8e
chanima <1670715459@qq.com>
2017-03-22 18:09:52 +0800

    appliction

--- app/src/main/java/com/example/win7/xiangmu_jicheng/MainActivity.java
+++ app/src/main/java/com/example/win7/xiangmu_jicheng/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.win7.xiangmu_jicheng;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.hyphenate.EMCallBack;
+import com.hyphenate.chat.EMClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +13,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        DenguHuidiao();
+    }
 
+    private void DenguHuidiao() {
+        EMClient.getInstance().login("zhanglei521","521521",new EMCallBack() {//回调
+            @Override
+            public void onSuccess() {
+                EMClient.getInstance().groupManager().loadAllGroups();
+                EMClient.getInstance().chatManager().loadAllConversations();
+                Log.e("main", "登录聊天服务器成功！");
+            }
+
+            @Override
+            public void onProgress(int progress, String status) {
+
+            }
+
+            @Override
+            public void onError(int code, String message) {
+                Log.e("main", "登录聊天服务器失败！");
+            }
+        });
     }
 }
